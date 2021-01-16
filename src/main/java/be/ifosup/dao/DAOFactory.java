af0d7d4acd986231e7afef48a2f8590c235a19d1
Arckanins <wow_Haukins@hotmail.com>
2021-01-16 09:05:23 +0100

    base de la DAO Factory

--- src/main/java/be/ifosup/dao/DAOFactory.java
+++ src/main/java/be/ifosup/dao/DAOFactory.java
@@ -1,4 +1,44 @@
 package be.ifosup.dao;
 
+
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class DAOFactory {
+    // Attributs
+    private final String url;
+    private final String username;
+    private final String password;
+
+    // Constructeur
+
+    public DAOFactory(String url, String username, String password){
+        this.url = url;
+        this.username = username;
+        this.password = password;
+    }
+
+    // Charger le driver et se connecter a la bdd
+
+    public static DAOFactory getInstance(){
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        } catch (ClassNotFoundException e){
+            e.printStackTrace();
+        }
+        DAOFactory instance = new DAOFactory("jdbc:mysql://localhost:3306/poo", "root", "");
+        return instance;
+    }
+
+    // Recuperer la connexion
+
+    public Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(url, username, password);
+    }
+
+    // Recuperer les données
+
+
 }
