7252442569fc5b169a07b3e960610ebf07830c20
pipartde <denis.pipart@gmail.com>
2021-01-23 13:05:12 +0100

    début du crud produit

--- src/main/java/be/ifosup/servlet/ServletProduitAdd.java
+++ src/main/java/be/ifosup/servlet/ServletProduitAdd.java
@@ -1,16 +1,22 @@
 package be.ifosup.servlet;
 
+import be.ifosup.categories.Categories;
 import be.ifosup.categories.CategoriesDAO;
 import be.ifosup.dao.DAOFactory;
+import be.ifosup.magasin.MagasinDAO;
 import be.ifosup.mesure.MesureDAO;
 import be.ifosup.produit.Produit;
 import be.ifosup.produit.ProduitDAO;
 
-import javax.servlet.*;
-import javax.servlet.http.*;
-import javax.servlet.annotation.*;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 @WebServlet(name = "ServletProduitAdd", urlPatterns = {"/add-produit"})
 public class ServletProduitAdd extends HttpServlet {
@@ -21,6 +27,8 @@ public class ServletProduitAdd extends HttpServlet {
     public void init() throws ServletException {
         DAOFactory daoFactory = DAOFactory.getInstance();
         this.produitDAO = daoFactory.getProduitsDAO();
+        this.categoriesDAO = daoFactory.getCategoriesDAO();
+        this.mesureDAO = daoFactory.getMesuresDAO();
     }
 
 
@@ -29,8 +37,10 @@ public class ServletProduitAdd extends HttpServlet {
         // servlet envoi des données dans une page .jsp
 
         try {
+
             request.setAttribute("categories", categoriesDAO.liste());
             request.setAttribute("mesures", mesureDAO.liste());
+
         } catch (SQLException throwables) {
             throwables.printStackTrace();
         }
@@ -38,9 +48,12 @@ public class ServletProduitAdd extends HttpServlet {
         request.getRequestDispatcher("vues/produitAdd.jsp").forward(request, response);
     }
 
+
+
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         // servlet reçoit les info formulaire
+
         // forcer l'UTF-8 dans les échanges
         request.setCharacterEncoding("UTF-8");
 
--- src/main/webapp/vues/produitAdd.jsp
+++ src/main/webapp/vues/produitAdd.jsp
@@ -1,38 +1,32 @@
-<%--
-  Created by IntelliJ IDEA.
-  User: denis
-  Date: 23/01/2021
-  Time: 09:32
-  To change this template use File | Settings | File Templates.
---%>
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
-<html>
-<head>
-    <title>Ajouter un produit</title>
-</head>
-<body>
-<h4>Ajout d'un produit</h4>
-<form action="produitAdd" method="post">
 
-    <select name="categorie">
-        <c:forEach items="${categories}" var="cat">
-            <option value="${cat.catId}">${cat.catNom}</option>
-        </c:forEach>
-    </select>
+<%@include file="../templates/header.jsp"%>
+<%@include file="../templates/navigation.jsp"%>
+<div class="container">
+    <main>
+        <h4>Ajout d'un produit</h4>
+        <form action="produitAdd" method="post">
+            <select name="category">
+                <c:forEach items="${categories}" var="cat">
+                    <option value="${cat.catId}">${cat.catNom}</option>
+                </c:forEach>
+            </select>
 
-    <input type="text" name="proNom" placeholder="produit">
+            <input type="text" name="proNom" placeholder="produit">
 
-    <select name="mesure">
-        <c:forEach items="${mesures}" var="mesure">
-            <option value="${mesure.mesId}">${mesure.mesNom}</option>
-        </c:forEach>
-    </select>
+            <select name="mesures">
+                <c:forEach items="${mesures}" var="mesure">
+                    <option value="${mesure.mesId}">${mesure.mesNom}</option>
+                </c:forEach>
+            </select>
 
-    <input type="text" name="quantity" placeholder="Quantity">
+            <input type="text" name="quantity" placeholder="Quantity">
 
 
-    <input type="submit" value="Ajouter">
-</form>
+            <input type="submit" value="Ajouter">
+            <p></p>
+        </form>
+    </main>
+</div>
 
-</body>
-</html>
+<%@include file="../templates/footer.jsp"%>
